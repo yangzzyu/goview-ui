59c49f274d4d0cc0085c7c362bf99fc448379fa7
yangyu <1431330771@qq.com>
2024-12-19 16:27:29 +0800

    修改demo

---
+++ demo.vue
@@ -0,0 +1,19 @@
+<!--
+ * @Author: yangyu 1431330771@qq.com
+ * @Date: 2024-10-09 09:04:29
+ * @LastEditors: yangyu 1431330771@qq.com
+ * @LastEditTime: 2024-12-19 14:46:02
+ * @FilePath: \MY-Kit-main\packages\GoView\docs\demo.vue
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+-->
+<template>
+  <div style="overflow: auto;">
+    <goView :dataJson="dataJson"></goView>
+  </div>
+</template>
+
+<script setup>
+import dataJson from "./1716877101106.json";
+
+// const onClick = (num) => { console.log(`我是第 ${num} 个自定义按钮`) }
+</script>
